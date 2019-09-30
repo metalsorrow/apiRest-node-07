cb4a8bc965ee2316f5da379690e3be58ff92b580
Marcos Laptop <marc.fuentesa@alumnos.duoc.cl>
2019-09-30 01:30:14 -0300

    Add, New validation Connect

--- server/config/config.js
+++ server/config/config.js
@@ -18,6 +18,6 @@ let urlDB;
 // if ( process.env.NODE_ENV === 'dev' ){
 //     urlDB= 'mongodb://localhost:27017/cafe'
 // }else{
-    urlDB ='mongodb+srv://Tauz:Pg4sPX6YiRs7oGP9@cluster0-pnipy.mongodb.net/cafe'
+    urlDB = process.env.URL
 // }
 process.env.URLDB = urlDB
