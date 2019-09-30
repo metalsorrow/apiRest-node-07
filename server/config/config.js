516c9d75d380b14234e7d284277e11ada83d2661
Marcos Laptop <marc.fuentesa@alumnos.duoc.cl>
2019-09-30 01:36:53 -0300

    Fix err, URL

--- server/config/config.js
+++ server/config/config.js
@@ -18,6 +18,6 @@ let urlDB;
 // if ( process.env.NODE_ENV === 'dev' ){
 //     urlDB= 'mongodb://localhost:27017/cafe'
 // }else{
-    urlDB = process.env.URL
+    urlDB = process.env.MONGO_URL
 // }
 process.env.URLDB = urlDB
